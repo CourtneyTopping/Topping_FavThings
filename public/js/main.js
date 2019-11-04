8ad8f95aa581ff5c8065ea6a030455c60817d178
Courtney Topping <Courtney@Courtneys-MacBook-Pro.local>
2019-11-04 14:28:37 -0500

    file changes

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,61 @@
 
 const myVM = (() => {
     // get the user buttons and fire off an async DB query with Fetch
+    let userButtons = document.querySelectorAll('.u-link'),
+        lightbox = document.querySelector('.lightbox');
+
+        function renderSocialMedia(socialMedia) {
+            return `<ul class="u-social">
+                ${socialMedia.map(item => `<li>${item}</li>`).join('')}
+            </ul>`
+        }
+
+        function songInfo(songs) { // person is the databaase result
+            let targetDiv = document.querySelector('.lb-content'),
+                targetImg = lightbox.querySelector('img');
+
+                let songContent = `
+                <h2>${songs.Song}</h2>
+                <h3>${songs.Artist}</h3>
+                <h4>${songs.Album}</h4>
+                <p>${songs.Genre}</p>
+                <p>${songs.About}</p>
+                `
+
+            console.log(songContent);
+
+            targetDiv.innerHTML = songContent;
+            targetImg.src = songs.imgsrc;
+
+            lightbox.classList.add('show-lb');
+
+
+        }
+
+        function getUserData(event) {
+            event.preventDefault(); //kill default tag in behaviour (dont navigate anywhere)
+           // debugger;
+            let imgSrc = this.previousElementSibling.getAttribute('src');
+            let url = `/${this.getAttribute('href')}`; // /1
+
+            fetch(url) // go get data
+                .then(res => res.json()) // parse the json into a plain object
+                .then(data => {
+                    console.log("my database result is:", data)
+
+                    data[0].imgsrc = imgSrc;
+
+                    songInfo(data[0]);
+                })
+                .catch((err) => {
+                    console.log(err)
+                })}
+
+        userButtons.forEach(button => button.addEventListener('click', getUserData));
+
+        lightbox.querySelector('.close').addEventListener('click', function() {
+            lightbox.classList.remove('show-lb');
+
+        });
 
 })();
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,46 @@ router.get('/', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
 
-    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card";
+    let query = "SELECT ID, Image, Song, Artist, Genre FROM tbl_songs";
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        //console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        res.render('home', { people: result });
+    
+    })
+})
+//localhost:3000
+router.get('/:id', (req, res) => {
+    console.log('hit a dynamic route!');
+    console.log(req.params.id);
+
+    let query = `SELECT * FROM tbl_songs WHERE ID="${req.params.id}"`;
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
         console.log(result); // should see objects wrapped in an array
 
+        // turn our social property insto an array - its just text in the DB,
+        // which isnt really anything we can work with
+       // result[0].social = result[0].social.split(",").map(function(item) {
+            //item = item.trim(); // remove the extra spaces for each word
+
+            //return item;
+        })
+
+       // console.log('after split: ', result[0]);
+
+        // send DB query back to browser
         // render the home view with dynamic data
-        res.render('home', { data: result });
+        res.json(result);
+    
     })
-})
+
+
 
 module.exports = router;
